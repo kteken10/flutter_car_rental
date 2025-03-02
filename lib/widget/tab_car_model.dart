ed1cd9fd199cb0cd34ac81af075b797c207bcea5
Dissang Tenkam Francis Kevin <dissangfrancis@yahoo.com>
2025-03-02 04:42:04 +0100

    UPDATING HOME SCREEN

--- lib/widget/tab_car_model.dart
+++ lib/widget/tab_car_model.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:car_rental_app/constants.dart';
 
-class TabCarModel extends StatelessWidget {
+class TabCarModel extends StatefulWidget {
+  TabCarModel({super.key});
+
+  @override
+  _TabCarModelState createState() => _TabCarModelState();
+}
+
+class _TabCarModelState extends State<TabCarModel> {
   final List<Map<String, String>> carModels = [
     {'name': 'All', 'icon': ''},
     {'name': 'Ferrari', 'icon': 'assets/ferrari_logo.svg'},
@@ -11,7 +18,7 @@ class TabCarModel extends StatelessWidget {
     {'name': 'Toyota', 'icon': 'assets/toyota_logo.svg'},
   ];
 
-  TabCarModel({super.key});
+  String selectedModel = 'All';
 
   @override
   Widget build(BuildContext context) {
@@ -19,31 +26,44 @@ class TabCarModel extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       child: Row(
         children: carModels.map((model) {
-          return Container(
-            margin: const EdgeInsets.symmetric(horizontal: 8.0),
-            padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(color: AppColors.grayFineColor, width: 0.25),
-            ),
-            child: Row(
-              children: [
-                if (model['icon']!.isNotEmpty)
-                  SvgPicture.asset(
-                    model['icon']!,
-                    width: 24,
-                    height: 24,
+          bool isSelected = model['name'] == selectedModel;
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedModel = model['name']!;
+              });
+            },
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 8.0),
+              padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              decoration: BoxDecoration(
+                color: isSelected ? Colors.yellow : Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color: AppColors.grayFineColor, width: 0.25),
+              ),
+              child: Row(
+                children: [
+                  if (model['icon']!.isNotEmpty)
+                    SvgPicture.asset(
+                      model['icon']!,
+                      width: 24,
+                      height: 24,
+                      fit: BoxFit.contain,
+
+                   
+                      
+                    ),
+                  if (model['icon']!.isNotEmpty) const SizedBox(width: 10),
+                  Text(
+                    model['name']!,
+                    style: const TextStyle(
+                      color: AppColors.ternaryColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12
+                    ),
                   ),
-                if (model['icon']!.isNotEmpty) const SizedBox(width: 10),
-                Text(
-                  model['name']!,
-                  style: const TextStyle(
-                    color: AppColors.ternaryColor,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           );
         }).toList(),
